eb78126444e233de3acf8252203a0bff17a8d818
Erik Beem <erik.beem@hotmail.com>
2021-10-20 16:50:07 +0200

    code refactor

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	console.log('Whats up');
+</script>
+
+<svelte:head>
+	<title>Portfolio</title>
+</svelte:head>
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to my portfolio website.</h1>
