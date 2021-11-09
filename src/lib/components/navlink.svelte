b575ba29a602a01f7455443c0767c06cd7d9d64a
Erik Beem <erik.beem@hotmail.com>
2021-11-09 17:29:09 +0100

    refactor

--- src/lib/components/navlink.svelte
+++ src/lib/components/navlink.svelte
@@ -4,6 +4,13 @@
 </script>
 
 <a
-	class="mx-4 text-lg text-gray-500 active:text-gray-900 hover:text-red-600 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"
+	class="mx-4 text-lg 
+    text-gray-500 
+    active:text-gray-900 
+    hover:text-red-600 
+    focus:ring-red-600 
+    focus:ring-2
+    focus:outline-none 
+    focus:ring-opacity-50"
 	href={linkAddr}>{linkName}</a
 >
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -5,7 +5,6 @@
 <svelte:head>
 	<title>Blog</title>
 </svelte:head>
-<h1 class="text-4xl text-center my-8">hi, hello</h1>
 
 <h1 class="text-4xl text-center my-8 uppercase">Posts</h1>
 
