d9221a560242a08b3ce30ecc2552a4cc3eb562b3
Erik Beem <erik.beem@hotmail.com>
2021-11-10 16:07:37 +0100

    feat: add hero component

---
+++ src/lib/components/hero.svelte
@@ -0,0 +1,16 @@
+<div class="text-center min-h-screen bg-metal-400 p-32">
+	<h1 class="font-sans text-5xl font-bold text-white mb-12">noob student</h1>
+
+	<div class="space-x-8">
+		<a
+			href="/contact"
+			class="py-4 px-8 rounded-md text-white bg-lavender-600 hover:bg-bluepurple-400 hover:shadow-xl transition duration-300"
+			>Hire me</a
+		>
+		<a
+			href="/work"
+			class="py-4 px-8 rounded-md  text-white bg-lavender-800 hover:bg-lavender-500 hover:shadow-xl transition duration-300"
+			>Please</a
+		>
+	</div>
+</div>
---
+++ src/routes/test/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Hero from '@components/hero.svelte';
+</script>
+
+<svelte:head>
+	<title>Portfolio</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8">hi, hello</h1>
+<Hero />
