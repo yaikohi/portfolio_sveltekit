b2779dbe6ba76843c3b65dd06ab87c4151d46c69
Erik Beem <erik.beem@hotmail.com>
2021-11-10 16:07:47 +0100

    feat: add contact page

---
+++ src/routes/contact/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<h1>Contact me!</h1>
+<div>
+	<div>
+		<h2>External links</h2>
+	</div>
+	<div>
+		<ul>
+			<li>
+				<div class="icon" />
+				<a href="https://www.linkedin.com/in/erik-beem-3a245a117/">LinkedIn</a>
+			</li>
+			<li>
+				<div class="icon" />
+				<a href="https://www.instagram.com/hhhghq/">Instagram</a>
+			</li>
+			<li>
+				<div class="icon" />
+				<a href="https://twitter.com/i0082425">Twitter</a>
+			</li>
+		</ul>
+	</div>
+</div>
